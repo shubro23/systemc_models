c846050588027bd695b1485e9b73b7dfed6ffea6
shubro <461shubro@gmail.com>
2026-01-20 10:16:14 +0000

    read/write both end from cpu to mem

--- tlm_writefromcputomemory.cpp
+++ tlm_writefromcputomemory.cpp
@@ -32,7 +32,16 @@ void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay){
       else{
         trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
       }
+    }else if (cmd == tlm::TLM_READ_COMMAND) { // ADD THIS BACK
+        if (adr < 64) {
+            // Copy FROM Target memory TO the Initiator's pointer
+            memcpy(ptr, &mem[adr], trans.get_data_length());
+            trans.set_response_status(tlm::TLM_OK_RESPONSE);
+        } else {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+        }
     }
+  
 } 
   };
   
@@ -46,14 +55,24 @@ void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay){
     void thread_process(){
       tlm::tlm_generic_payload *trans= new tlm::tlm_generic_payload;
       sc_time delay=sc_time(10,SC_NS);
+
+
+      //for write to memory CPU -> MEMORY
       int data =101;
-      
       trans->set_command(tlm::TLM_WRITE_COMMAND);
       trans->set_address(0x1F);
       trans->set_data_ptr(reinterpret_cast<unsigned char*>(&data));// sharing the data pointer of where the date is being stored!
       trans->set_data_length(4);
-      
       isocket->b_transport(*trans, delay);
+
+      //Read from memory to cpu mem->CPU
+      int read_data=0;
+      
+      trans->set_command(tlm::TLM_READ_COMMAND);
+      trans->set_data_ptr(reinterpret_cast<unsigned char*>(&read_data));
+      trans->set_data_length(4);
+      isocket->b_transport(*trans,delay);
+      std::cout << "INITIATOR: Read back value " << read_data << " from Target!" << std::endl;
     }
   };
   int sc_main(int argc, char* argv[]) {
